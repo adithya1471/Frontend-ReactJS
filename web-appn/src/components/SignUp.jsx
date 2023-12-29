1e03bf8d7185aa3503bbb09d7ca619b099c11301
adithyav7777@gmail.com <adithyav7777@gmail.com>
2023-12-29 22:30:18 +0530

    Front end  project

---
+++ web-appn/src/components/SignUp.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './SignUp.css'
+import NavBar from './NavBar'
+
+const SignUp=()=>{
+    
+    return(
+        <>  
+        <NavBar />
+        <div className='main-head'>
+            
+        </div>
+         </> 
+    )
+}
+export default SignUp
